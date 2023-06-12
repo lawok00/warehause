e07751e2f0b91a131c253f7bf57ee5fe07d8dbcd
lawok <lawok00@op.pl>
2023-06-12 19:44:49 +0200

    start

---
+++ src/main/java/com/warehause/warehause/WarehauseApplication.java
@@ -0,0 +1,13 @@
+package com.warehause.warehause;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehauseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehauseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warehause/warehause/WarehauseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehause.warehause;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehauseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
